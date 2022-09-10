ceb6f21c2fc292508a78c97ab71ad164f6e6ee82
FaridBF <bouras.farid@outlook.fr>
2022-09-10 19:58:12 +0200

    feat: close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+function closeModal() {
+  //méthode qui permet de supprimer un style dans le dom
+  modalbg.removeAttribute("style")
+}
